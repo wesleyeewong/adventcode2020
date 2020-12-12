64a42f81f7ef73d0ef6237c65d8abf3f66de7645
wesley wong <wesley@freeletics.com>
2020-12-12 15:12:20 +0100

    add template

---
+++ template.rb
@@ -0,0 +1,18 @@
+def solve_part_1
+  File.open("./question_10_input.txt", "r") do |file|
+    file.each_line do |line|
+
+    end
+  end
+end
+
+def solve_part_2
+  File.open("./question_10_input.txt", "r") do |file|
+    file.each_line do |line|
+
+    end
+  end
+end
+
+solve_part_1
+solve_part_2
